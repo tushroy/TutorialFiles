554a359e08fabb33343ba9a5c14af929d21146bf
Tushar Roy <xxcrack@gmail.com>
2013-12-25 17:33:58 +0600

    Calculate pi monte carlo method

---
+++ Calcpi.java
@@ -0,0 +1,16 @@
+public class Calcpi {
+    public static void main(String[] args) {
+        double x, y;
+        int insideCircle = 0, total = 999999;//Integer.MAX_VALUE; //MAX_VALUE will need a lots of time but will yeild better result
+        for (int i = 0; i < total; i++) {
+            x = Math.random();
+            y = Math.random();
+            if (x * x + y * y <= 1){
+            	insideCircle++;
+            	System.out.print("Percent "+Math.round((100.0*i)/total)+"\r");
+            }
+        }
+        System.out.println("\n"+(4.0 * insideCircle / total)+" as i calculated");
+        System.out.println(Math.PI+" as the system uses");
+    }
+}
